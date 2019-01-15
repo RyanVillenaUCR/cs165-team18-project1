e6fab14d9a90a10413c7531bd97161b1dd4bb4a2
Ryan Villena <rvill028@wch133-02.cs.ucr.edu>
2019-01-15 10:36:13 -0800

    Wrote skeleton and some tests
    * Wrote function which iterates 'a' thru 'zzzzz'
    * Wrote main() which will call everything
    * TODO verify that md5.digest works

---
+++ cracker.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python3
+
+import hashlib
+
+#print("hello world")
+
+def grabFile(fileName):
+  textFile = ""
+
+  with open(fileName) as file:
+    textFile = file.read()
+
+  return textFile
+
+def tryString(attempted_string, salt, correct_hash):
+
+  m = hashlib.md5()
+
+  m.update(attempted_string)
+  m.update(salt)
+
+  return m.digest() == correct_hash
+
+
+
+
+def crack(salt, correct_hash):
+  return "temp_crack_retval"
+  
+def test_tryString():
+
+  print(tryString("test", "001", "fa820cc1ad39a4e99283e9fa555035ec"))
+
+def test_iters():
+
+  #try printing all chars from a-z
+  #for i in range(ord('a'), ord('z') + 1):
+  #  print(chr(i))
+  #  print("\n")
+
+  #now, try printing all chars from a-zzz
+  for i1 in range(ord('a'), ord('z') + 1):
+    
+    s1 = chr(i1)
+    print(s1)
+
+    for i2 in range(ord('a'), ord('z') + 1):
+
+      s2 = s1 + chr(i2)
+      print(s2)
+
+      for i3 in range(ord('a'), ord('z') + 1):
+
+        s3 = s2 + chr(i3)
+        print(s3)
+
+def main():
+
+  test_tryString()
+
+  salt = "hfT7jp2q"
+  correct_hash = "JU0X9xRQyTWTWY59e3Iqj1"
+
+  pw = crack(salt, correct_hash)
+
+  with open("result.txt", 'w') as file:
+
+    file.write(pw)
+
+
+
+
+if __name__ == "__main__":
+  main()
+
